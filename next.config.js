570ae27fcc009432397af57d34c61f6efca5b7e6
Saswat S. Patra <84391114+saswatpatra@users.noreply.github.com>
2025-01-04 23:55:26 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  // Uncomment and modify the following line if you're deploying to a subdirectory
+  // basePath: '/bird-game',
+}
+
+module.exports = nextConfig
