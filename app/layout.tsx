52e25c8edbf296d69bfcad9e34e43acd0808f597
Saswat S. Patra <84391114+saswatpatra@users.noreply.github.com>
2025-01-18 20:03:55 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,11 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Bird Game',
   description: 'A fun game to test your bird knowledge',
+  icons: {
+    icon: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/insta%20dp%20(1)-yFqQWtusAztzvehNnpL21qmjqytbXa.png',
+    shortcut: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/insta%20dp%20(1)-yFqQWtusAztzvehNnpL21qmjqytbXa.png',
+    apple: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/insta%20dp%20(1)-yFqQWtusAztzvehNnpL21qmjqytbXa.png',
+  },
 }
 
 export default function RootLayout({
@@ -20,7 +25,7 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/insta%20dp%20(1)-yFqQWtusAztzvehNnpL21qmjqytbXa.png" />
       </head>
       <body>
         {children}
@@ -29,3 +34,4 @@ export default function RootLayout({
     </html>
   )
 }
+
