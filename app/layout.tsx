70accc989e27bdb2f07f9ccefacf11c3e32cf1fc
Saswat S. Patra <84391114+saswatpatra@users.noreply.github.com>
2025-01-24 23:32:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,34 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: 'Bird Game',
-  description: 'A fun game to test your bird knowledge',
+  title: "Bird Tap Game",
+  description: "Digital version of the infamous Chidiya Udd game",
+  openGraph: {
+    title: "Bird Tap Game",
+    description: "Digital version of the infamous Chidiya Udd game",
+    url: "https://birdtap.vercel.app",
+    siteName: "Bird Tap Game",
+    images: [
+      {
+        url: "https://birdtap.vercel.app/og-image.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
   icons: {
-    icon: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/insta%20dp%20(1)-yFqQWtusAztzvehNnpL21qmjqytbXa.png',
-    shortcut: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/insta%20dp%20(1)-yFqQWtusAztzvehNnpL21qmjqytbXa.png',
-    apple: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/insta%20dp%20(1)-yFqQWtusAztzvehNnpL21qmjqytbXa.png',
+    icon: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/insta%20dp%20(1)-yFqQWtusAztzvehNnpL21qmjqytbXa.png",
+    shortcut:
+      "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/insta%20dp%20(1)-yFqQWtusAztzvehNnpL21qmjqytbXa.png",
+    apple:
+      "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/insta%20dp%20(1)-yFqQWtusAztzvehNnpL21qmjqytbXa.png",
   },
 }
 
@@ -25,7 +42,10 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/insta%20dp%20(1)-yFqQWtusAztzvehNnpL21qmjqytbXa.png" />
+        <link
+          rel="icon"
+          href="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/insta%20dp%20(1)-yFqQWtusAztzvehNnpL21qmjqytbXa.png"
+        />
       </head>
       <body>
         {children}
