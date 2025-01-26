f140de5f1becab707737d25a9f4ae120ba3c4315
Saswat S. Patra <84391114+saswatpatra@users.noreply.github.com>
2025-01-26 20:57:41 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com", "github.githubassets.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
-      },
-      {
-        protocol: "https",
-        hostname: "github.githubassets.com",
-        pathname: "/images/**",
-      },
-    ],
   },
 }
 
